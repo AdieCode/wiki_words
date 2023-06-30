ac9a7d45e1e59f03d431d8066e87f7210b6da4b1
Adie_code <Adiecode4@gmail.com>
2023-06-30 22:58:44 +0200

    adding pyton scraper code and progress loader

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from bs4 import BeautifulSoup
 import requests 
-
-page_to_scrape = requests.get("http://quotes.toscrape.com/")
-sooup = BeautifulSoup(page_to_scrape.text, "html.parser")
-qouts = sooup.find_all("span", attrs={"class":"text"})
-authors = sooup.find_all("small", attrs={"class":"author"})
+import sys
 
 lst = []
-for i in qouts:
-    lst.append(i.text)
+count = 0
+divider = 0
+print("progress : [",end="",flush=True)
 
-print(lst)
+none_found = 0
+while count < 100:
 
+    try :
+        page_to_scrape = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+        sooup = BeautifulSoup(page_to_scrape.text, "html.parser")
+        paragraph = sooup.find_all("p")
+        if paragraph and len(paragraph[0].text) >8:
+            count += 1
+            lst.append(paragraph[0].text)
+        if count > divider:
+            divider += 10
+            print("█",end="",flush=True)
+    except:
+         none_found += 1
+         
+
+print("]__100%__ " +(sys.getsizeof(lst) + " bytes"))
+
+print("None found : " + str(none_found))
+
+with open("info.txt", "a", encoding="utf-8") as file:
+    for par in lst:
+        if len(par) > 0:
+            file.write(par)
 
---
+++ printer.py
@@ -0,0 +1,7 @@
+
+import time
+
+for i in range(1, 11):
+    print(i, end='\r')
+    time.sleep(1)
+    
